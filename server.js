3675c198b494bbd2b08dab7094c3a82da9823ac3
Daniel Parker <dparker.tech@gmail.com>
2016-09-30 22:16:40 +1000

    procfile updates

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var path = require("path");
 var bodyParser = require("body-parser");
 
 var app = express();
-app.use(express.static(__dirname + "/public"));
+app.use(express.static(__dirname + "/dist"));
 app.use(bodyParser.json());
 
 var server = app.listen(process.env.PORT || 8080, function () {
